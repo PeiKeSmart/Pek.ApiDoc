d0935dfc75d911062d3c5516b2ff9c997ebca792
猿人易 <qcjxberin@outlook.com>
2025-06-20 17:52:33 +0800

    重构路由配置，使用 Pek 路由器
    在 `Program.cs` 中，将 `app.UseCube` 替换为 `app.UsePekRouter`，并映射控制器路由。注释掉 `app.MapControllers()`，以通过 Pek 路由器处理路由。

--- ApiDocDemo/Program.cs
+++ ApiDocDemo/Program.cs
@@ -118,12 +118,13 @@ app.UseAuthentication();  // 认证中间件 用于Jwt检验
 //app.UseRouting(); // 路由中间件
 app.UsePekRouter(endpoints =>
 {
+    endpoints.MapControllers(); // 映射控制器路由
     app.UsePekEndpoints(); // 注册 Pek 的路由端点
 });
 
 app.UseAuthorization();  // 授权中间件
 
-app.MapControllers();
+//app.MapControllers();
 //app.UseCubeHome();
 
 //app.RegisterService("DHDeploy.Agent", null, builder.Environment.EnvironmentName, "/pek/info");
